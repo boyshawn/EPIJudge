16c2f2151210e471ce6519301993b2a2083866bf
boyshawn <boyshawn@hotmail.com>
2020-11-23 08:13:05 +0800

    6.18 Rotate A 2D Array

--- epi_judge_java/epi/MatrixRotation.java
+++ epi_judge_java/epi/MatrixRotation.java
@@ -6,12 +6,35 @@ import java.util.List;
 public class MatrixRotation {
 
   public static void rotateMatrix(List<List<Integer>> squareMatrix) {
-    // TODO - you fill in here.
+    int midPoint = squareMatrix.size() / 2;
+    for (int row = 0; row < midPoint; row++) {
+      int boundaryRow = squareMatrix.size() - row - 1;
+      int firstColumn = row;
+      int lastColumn = boundaryRow;
+      while (firstColumn < boundaryRow) {
+        fourWaySwap(squareMatrix, row, firstColumn, boundaryRow, lastColumn);
+        lastColumn--;
+        firstColumn++;
+      }
+    }
     return;
   }
+
+  private static void fourWaySwap(List<List<Integer>> squareMatrix, int coorindate1, int coorindate2, int coorindate3,
+                                  int coorindate4) {
+    int value1 = squareMatrix.get(coorindate1).get(coorindate2);
+    int value2 = squareMatrix.get(coorindate2).get(coorindate3);
+    int value3 = squareMatrix.get(coorindate3).get(coorindate4);
+    int value4 = squareMatrix.get(coorindate4).get(coorindate1);
+
+    squareMatrix.get(coorindate1).set(coorindate2, value4);
+    squareMatrix.get(coorindate2).set(coorindate3, value1);
+    squareMatrix.get(coorindate3).set(coorindate4, value2);
+    squareMatrix.get(coorindate4).set(coorindate1, value3);
+  }
+
   @EpiTest(testDataFile = "matrix_rotation.tsv")
-  public static List<List<Integer>>
-  rotateMatrixWrapper(List<List<Integer>> squareMatrix) {
+  public static List<List<Integer>> rotateMatrixWrapper(List<List<Integer>> squareMatrix) {
     rotateMatrix(squareMatrix);
     return squareMatrix;
   }
@@ -20,7 +43,7 @@ public class MatrixRotation {
     System.exit(
         GenericTest
             .runFromAnnotations(args, "MatrixRotation.java",
-                                new Object() {}.getClass().getEnclosingClass())
+                new Object() {}.getClass().getEnclosingClass())
             .ordinal());
   }
 }
