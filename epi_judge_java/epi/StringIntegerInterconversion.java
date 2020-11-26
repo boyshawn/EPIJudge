0738b8bb113e46c924b3d95af591418df49b18c5
boyshawn <boyshawn@hotmail.com>
2020-11-26 09:52:50 +0800

    7.1 Interconvert Strings and Integers

--- epi_judge_java/epi/StringIntegerInterconversion.java
+++ epi_judge_java/epi/StringIntegerInterconversion.java
@@ -5,13 +5,62 @@ import epi.test_framework.TestFailure;
 public class StringIntegerInterconversion {
 
   public static String intToString(int x) {
-    // TODO - you fill in here.
-    return "0";
+    StringBuilder integerStringBuilder = new StringBuilder();
+    if (x == 0) {
+      return "0";
+    }
+    if (x == Integer.MIN_VALUE) {
+      return "-2147483648";
+    }
+    boolean isNegative = (x < 0);
+    if (isNegative) {
+      x *= -1;
+    }
+    while (x > 0) {
+      int reminder = x % 10;
+      integerStringBuilder.append(reminder);
+      x /= 10;
+    }
+    if (isNegative) {
+      integerStringBuilder.append('-');
+    }
+    String integer = reverseStringBuilder(integerStringBuilder);
+    return integer;
   }
+
+  private static String reverseStringBuilder(StringBuilder integerStringBuilder) {
+    StringBuilder stringBuilder = new StringBuilder();
+    for (int i = integerStringBuilder.length() - 1; i >= 0; i--) {
+      stringBuilder.append(integerStringBuilder.charAt(i));
+    }
+    return stringBuilder.toString();
+  }
+
   public static int stringToInt(String s) {
-    // TODO - you fill in here.
-    return 0;
+    final int CODE_POINT_OFFSET = 48;
+    char firstCharacter = s.charAt(0);
+    boolean isFirstCharacterDigit = Character.isDigit(firstCharacter);
+    boolean isNegative = false;
+    if (!isFirstCharacterDigit) {
+      isNegative = checkForNegative(firstCharacter);
+    }
+    int parsingStartPoint = isFirstCharacterDigit ? 0 : 1;
+    int integer = 0;
+    int power = 0;
+    for (int i = s.length() - 1; i >= parsingStartPoint; i--) {
+      int codeValue = s.codePointAt(i);
+      int adjustedCodeValue = codeValue - CODE_POINT_OFFSET;
+      double value = adjustedCodeValue * Math.pow(10, power++);
+      integer += (int) value;
+    }
+    integer = (isNegative) ? -1 * integer : integer;
+    return integer;
   }
+
+  private static boolean checkForNegative(char firstCharacter) {
+    return firstCharacter == '-';
+  }
+
   @EpiTest(testDataFile = "string_integer_interconversion.tsv")
   public static void wrapper(int x, String s) throws TestFailure {
     if (Integer.parseInt(intToString(x)) != x) {
