481f10ddc54c695820b38057d5ba7b9d68969c7b
boyshawn <boyshawn@hotmail.com>
2020-12-01 08:55:12 +0800

    7.3 Compute the Spreadsheet Column Encoding

--- epi_judge_java/epi/SpreadsheetEncoding.java
+++ epi_judge_java/epi/SpreadsheetEncoding.java
@@ -5,8 +5,21 @@ public class SpreadsheetEncoding {
   @EpiTest(testDataFile = "spreadsheet_encoding.tsv")
 
   public static int ssDecodeColID(final String col) {
-    // TODO - you fill in here.
-    return 0;
+    final int ASCII_OFFSET = 64;
+    final int ALPHABET_NUMBER = 26;
+    final int INVALID_COLUMN_REPRESENTATION = 0;
+    if (col == null || col.isBlank()) {
+      return INVALID_COLUMN_REPRESENTATION;
+    }
+    int totalValue = 0;
+    for (int i = col.length() - 1; i >= 0; i--) {
+      int power = col.length() - 1 - i;
+      char character = col.charAt(i);
+      int characterValue = character - ASCII_OFFSET;
+      double value = characterValue * Math.pow(ALPHABET_NUMBER, power);
+      totalValue += (int) value;
+    }
+    return totalValue;
   }
 
   public static void main(String[] args) {
