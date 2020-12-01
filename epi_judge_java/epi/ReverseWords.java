c98e57fb1fde2ed987fb4c1197e6e2bf6a6157a9
boyshawn <boyshawn@hotmail.com>
2020-12-02 00:09:07 +0800

    7.6 Reverse All the Words in a Sentence - Answer is not optimised

--- epi_judge_java/epi/ReverseWords.java
+++ epi_judge_java/epi/ReverseWords.java
@@ -2,12 +2,36 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TimedExecutor;
+
+import java.util.ArrayList;
+
 public class ReverseWords {
 
   public static void reverseWords(char[] input) {
-    // TODO - you fill in here.
+    final char BLANK_SPACE = ' ';
+    StringBuilder stringBuilder = new StringBuilder();
+    ArrayList<Integer> blankSpacePositions = new ArrayList<>();
+    blankSpacePositions.add(-1);
+    for (int i = 0; i < input.length; i++) {
+      if (input[i] == BLANK_SPACE) {
+        blankSpacePositions.add(i);
+      }
+    }
+    int rightBoundary = input.length;
+    for (int index = blankSpacePositions.size() - 1; index >= 0; index--) {
+      int leftBoundary = blankSpacePositions.get(index);
+      for (int position = leftBoundary + 1; position < rightBoundary; position++) {
+        stringBuilder.append(input[position]);
+      }
+      stringBuilder.append(BLANK_SPACE);
+      rightBoundary = leftBoundary;
+    }
+    for (int i = 0; i < stringBuilder.length() - 1; i++) {
+      input[i] = stringBuilder.charAt(i);
+    }
     return;
   }
+
   @EpiTest(testDataFile = "reverse_words.tsv")
   public static String reverseWordsWrapper(TimedExecutor executor, String s)
       throws Exception {
