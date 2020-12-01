9f608cc93762deba513fa4e13c24e442444303ff
boyshawn <boyshawn@hotmail.com>
2020-12-01 09:50:12 +0800

    7.4 Replace and Remove

--- epi_judge_java/epi/IsStringPalindromic.java
+++ epi_judge_java/epi/IsStringPalindromic.java
@@ -5,8 +5,25 @@ public class IsStringPalindromic {
   @EpiTest(testDataFile = "is_string_palindromic.tsv")
 
   public static boolean isPalindromic(String s) {
-    // TODO - you fill in here.
-    return true;
+    int leftPosition = 0;
+    int rightPosition = s.length() - 1;
+    boolean isPalindromic = true;
+    while (leftPosition < rightPosition && isPalindromic) {
+      char leftCharacter = s.charAt(leftPosition);
+      if (!Character.isAlphabetic(leftCharacter)) {
+        leftPosition++;
+        continue;
+      }
+      char rightCharacter = s.charAt(rightPosition);
+      if (!Character.isAlphabetic(rightCharacter)) {
+        rightPosition--;
+        continue;
+      }
+      if (leftCharacter != rightCharacter) {
+        isPalindromic = false;
+      }
+    }
+    return isPalindromic;
   }
 
   public static void main(String[] args) {
