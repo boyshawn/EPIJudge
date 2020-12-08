bac7c21ab18677987d1c9ad769f31be555162540
boyshawn <boyshawn@hotmail.com>
2020-12-08 09:46:52 +0800

    7.9 Convert from Roman to Decimal

--- epi_judge_java/epi/RomanToInteger.java
+++ epi_judge_java/epi/RomanToInteger.java
@@ -1,12 +1,61 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class RomanToInteger {
   @EpiTest(testDataFile = "roman_to_integer.tsv")
-
   public static int romanToInteger(String s) {
-    // TODO - you fill in here.
-    return 0;
+    HashMap<Character, Integer> characterValues = new HashMap<>();
+    characterValues.put('I', 1);
+    characterValues.put('V', 5);
+    characterValues.put('X', 10);
+    characterValues.put('L', 50);
+    characterValues.put('C', 100);
+    characterValues.put('D', 500);
+    characterValues.put('M', 1000);
+    int totalValue = 0;
+    int charPosition = 0;
+    while (charPosition < s.length()) {
+      char character = s.charAt(charPosition);
+      char nextCharacter = (charPosition + 1 < s.length()) ? s.charAt(charPosition + 1) : ' ';
+      int characterValue = characterValues.get(character);
+      switch (character) {
+        case 'V':
+        case 'L':
+        case 'D':
+        case 'M':
+          totalValue += characterValue;
+          break;
+        case 'I':
+          if (nextCharacter == 'V' || nextCharacter == 'X') {
+            totalValue -= characterValue;
+          } else {
+            totalValue += characterValue;
+          }
+          break;
+        case 'X':
+          if (nextCharacter == 'L' || nextCharacter == 'C') {
+            totalValue -= characterValue;
+          } else {
+            totalValue += characterValue;
+          }
+          break;
+        case 'C':
+          if (nextCharacter == 'D' || nextCharacter == 'M') {
+            totalValue -= characterValue;
+          } else {
+            totalValue += characterValue;
+          }
+          break;
+        default:
+          throw new IllegalArgumentException(character + " is not allowed. string=" + s);
+      }
+      charPosition++;
+    }
+    return totalValue;
   }
 
   public static void main(String[] args) {
